5ab89b3614fca14708bbf3a314965c3b0bb21b5f
busrakk <busrakartal160@gmail.com>
2023-02-28 20:31:00 +0300

    added-authentication-api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// public
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+//protected
+Route::middleware(['auth:sanctum'])->group( function(){
+    Route::get('checkAuthenticated', function(){
+        return response()->json(['message' => 'You are in', 'status'=>200], 200);
+    });
+    Route::post('logout', [AuthController::class, 'logout']);
+});
